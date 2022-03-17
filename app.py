61fbe6af60d954580affc8b6d00fa7a61d23138e
Tomas Agustin Chacur <87044744+TomasChacur95@users.noreply.github.com>
2022-03-17 13:04:10 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,30 @@
+from  flask import Flask, render_template
+from flask_mysqldb import MySQL
+
+app = Flask(__name__)
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'password'
+app.config['MYSQL_DB'] = 'flask_contacts'
+
+mysql = MySQL(app)
+
+
+@app.route('/')
+def Index():
+    return  render_template('index.html')
+
+@app.route('/add_contact')
+def add_contact():
+    return 'welcome'
+
+@app.route('/edit')
+def edit_contact():
+    return 'edit contact'
+
+@app.route('/delete')
+def delete_contact():
+    return 'delete contact'
+
+if __name__ == '__main__':
+    app.run(port = 3000, debug = True)
